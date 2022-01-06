5a4bc8306b1161d4d9011adc0c7a58630bfffa58
emphasize <swen_g@t-online.de>
2022-01-06 12:58:59 +0100

    autoformat code

--- __init__.py
+++ __init__.py
@@ -13,15 +13,11 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
-from os.path import dirname
-
+import requests
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 from rapidfuzz import fuzz
-
-import requests
-import json
 
 # v 0.1 - just switch on and switch off a fix light
 # v 0.2 - code review
@@ -58,9 +54,9 @@ class openHABSkill(MycroftSkill):
 		self.switchableItemsDic = dict()
 		self.currentTempItemsDic = dict()
 		self.currentHumItemsDic = dict()
-		#self.currentThermostatItemsDic = dict()
 		self.targetTemperatureItemsDic = dict()
-		#self.homekitHeatingCoolingModeDic = dict()
+		# self.currentThermostatItemsDic = dict()
+		# self.homekitHeatingCoolingModeDic = dict()
 
 	def initialize(self):
 
@@ -71,28 +67,32 @@ class openHABSkill(MycroftSkill):
 			self.shutdown()
 
 		self.handle_websettings_update()
-		
-		if self.url is not None:
-		    self.getTaggedItems()
-		else:
-		    self.speak_dialog('ConfigurationNeeded')
 
-		refresh_tagged_items_intent = IntentBuilder("RefreshTaggedItemsIntent").require("RefreshTaggedItemsKeyword").build()
+		if self.url is not None:
+			self.getTaggedItems()
+		else:
+			self.speak_dialog('ConfigurationNeeded')
+
+		refresh_tagged_items_intent = IntentBuilder("RefreshTaggedItemsIntent").require(
+			"RefreshTaggedItemsKeyword").build()
 		self.register_intent(refresh_tagged_items_intent, self.handle_refresh_tagged_items_intent)
 
-		onoff_status_intent = IntentBuilder("OnOff_StatusIntent").require("OnOffStatusKeyword").require("Command").require("Item").build()
+		onoff_status_intent = IntentBuilder("OnOff_StatusIntent").require("OnOffStatusKeyword").require(
+			"Command").require("Item").build()
 		self.register_intent(onoff_status_intent, self.handle_onoff_status_intent)
 
-		dimmer_status_intent = IntentBuilder("Dimmer_StatusIntent").require("DimmerStatusKeyword").require("Item").optionally("BrightPercentage").build()
+		dimmer_status_intent = IntentBuilder("Dimmer_StatusIntent").require("DimmerStatusKeyword").require(
+			"Item").optionally("BrightPercentage").build()
 		self.register_intent(dimmer_status_intent, self.handle_dimmer_status_intent)
 
-		#what_status_intent = IntentBuilder("What_StatusIntent").require("WhatStatusKeyword").require("Item").require("RequestType").build()
-		#self.register_intent(what_status_intent, self.handle_what_status_intent)
+		# what_status_intent = IntentBuilder("What_StatusIntent").require("WhatStatusKeyword").require("Item").require("RequestType").build()
+		# self.register_intent(what_status_intent, self.handle_what_status_intent)
 		self.register_entity_file('item.entity')
 		self.register_entity_file('requesttype.entity')
-		self.register_intent_file('what.status.intent',self.handle_what_status_intent)
+		self.register_intent_file('what.status.intent', self.handle_what_status_intent)
 
-		setTemp_status_intent = IntentBuilder("SetTemp_StatusIntent").require("ThermostatStatusKeyword").require("Item").require("TempValue").build()
+		setTemp_status_intent = IntentBuilder("SetTemp_StatusIntent").require("ThermostatStatusKeyword").require(
+			"Item").require("TempValue").build()
 		self.register_intent(setTemp_status_intent, self.handle_setTemp_status_intent)
 
 		list_items_intent = IntentBuilder("ListItemsIntent").require("ListItemsKeyword").build()
@@ -108,11 +108,11 @@ class openHABSkill(MycroftSkill):
 			self.url = "http://%s:%s/rest" % (self.get_config('host'), self.get_config('port'))
 			self.getTaggedItems()
 		else:
-		    self.url = None
+			self.url = None
 
 	def getTaggedItems(self):
-		#find all the items tagged Lighting and Switchable from openHAB
-		#the labeled items are stored in dictionaries
+		# find all the items tagged Lighting and Switchable from openHAB
+		# the labeled items are stored in dictionaries
 
 		self.lightingItemsDic = {}
 		self.switchableItemsDic = {}
@@ -125,14 +125,14 @@ class openHABSkill(MycroftSkill):
 		if self.url == None:
 			LOGGER.error("Configuration needed!")
 			self.speak_dialog('ConfigurationNeeded')
-		else:			
-			requestUrl = self.url+"/items?recursive=false"
+		else:
+			requestUrl = self.url + "/items?recursive=false"
 
-			try: 
+			try:
 				req = requests.get(requestUrl, headers=self.polling_headers)
 				if req.status_code == 200:
 					json_response = req.json()
-					for x in range(0,len(json_response)):
+					for x in range(0, len(json_response)):
 						if ("Lighting" in json_response[x]['tags']):
 							self.lightingItemsDic.update({json_response[x]['name']: json_response[x]['label']})
 						elif ("Switchable" in json_response[x]['tags']):
@@ -146,7 +146,8 @@ class openHABSkill(MycroftSkill):
 						elif ("TargetTemperature" in json_response[x]['tags']):
 							self.targetTemperatureItemsDic.update({json_response[x]['name']: json_response[x]['label']})
 						elif ("homekit:HeatingCoolingMode" in json_response[x]['tags']):
-							self.homekitHeatingCoolingModeDic.update({json_response[x]['name']: json_response[x]['label']})
+							self.homekitHeatingCoolingModeDic.update(
+								{json_response[x]['name']: json_response[x]['label']})
 						else:
 							pass
 				else:
@@ -154,10 +155,10 @@ class openHABSkill(MycroftSkill):
 					self.speak_dialog('GetItemsListError')
 
 			except KeyError:
-						pass
+				pass
 			except Exception:
-					LOGGER.error("Some issues with the command execution!")
-					self.speak_dialog('GetItemsListError')
+				LOGGER.error("Some issues with the command execution!")
+				self.speak_dialog('GetItemsListError')
 
 	def findItemName(self, itemDictionary, messageItem):
 
@@ -172,10 +173,9 @@ class openHABSkill(MycroftSkill):
 					bestScore = score
 					bestItem = itemName
 		except KeyError:
-                    pass
+			pass
 
 		return bestItem
-
 
 	def getItemsFromDict(self, typeStr, itemsDict):
 		if len(itemsDict) == 0:
@@ -190,14 +190,18 @@ class openHABSkill(MycroftSkill):
 		msg = msg.strip() + ' ' + self.getItemsFromDict("Current Humidity", self.currentHumItemsDic) + "\n"
 		msg = msg.strip() + ' ' + self.getItemsFromDict("Thermostat", self.currentThermostatItemsDic) + "\n"
 		msg = msg.strip() + ' ' + self.getItemsFromDict("Target Temperature", self.targetTemperatureItemsDic) + "\n"
-		msg = msg.strip() + ' ' + self.getItemsFromDict("Homekit Heating and Cooling", self.homekitHeatingCoolingModeDic)
+		msg = msg.strip() + ' ' + self.getItemsFromDict("Homekit Heating and Cooling",
+														self.homekitHeatingCoolingModeDic)
 		self.speak_dialog('FoundItems', {'items': msg.strip()})
 
 	def handle_refresh_tagged_items_intent(self, message):
-		#to refresh the openHAB items labeled list we use an intent, we can ask Mycroft to make the refresh
+		# to refresh the openHAB items labeled list we use an intent, we can ask Mycroft to make the refresh
 
 		self.getTaggedItems()
-		dictLenght = str(len(self.lightingItemsDic) + len(self.switchableItemsDic) + len(self.currentTempItemsDic) + len(self.currentHumItemsDic) + len(self.currentThermostatItemsDic) + len(self.targetTemperatureItemsDic) + len(self.homekitHeatingCoolingModeDic))
+		dictLenght = str(
+			len(self.lightingItemsDic) + len(self.switchableItemsDic) + len(self.currentTempItemsDic) + len(
+				self.currentHumItemsDic) + len(self.currentThermostatItemsDic) + len(
+				self.targetTemperatureItemsDic) + len(self.homekitHeatingCoolingModeDic))
 		self.speak_dialog('RefreshTaggedItems', {'number_item': dictLenght})
 
 	def handle_onoff_status_intent(self, message):
@@ -205,7 +209,7 @@ class openHABSkill(MycroftSkill):
 		translate_command = self.translate_namedvalues('translate_command')
 		messageItem = message.data.get('Item')
 
-		#We have to find the item to update from our dictionaries
+		# We have to find the item to update from our dictionaries
 		self.lightingSwitchableItemsDic = dict()
 		self.lightingSwitchableItemsDic.update(self.lightingItemsDic)
 		self.lightingSwitchableItemsDic.update(self.switchableItemsDic)
@@ -241,28 +245,28 @@ class openHABSkill(MycroftSkill):
 		ohItem = self.findItemName(self.lightingItemsDic, messageItem)
 
 		if ohItem != None:
-			#if ((command == "set") or (command == "imposta") or (command == "setze") or (command == "pone")):
+			# if ((command == "set") or (command == "imposta") or (command == "setze") or (command == "pone")):
 			if self.voc_match(command, 'Set'):
 				if ((brightValue == None) or (int(brightValue) < 0) or (int(brightValue) > 100)):
 					self.speak_dialog('ErrorDialog')
 				else:
 					statusCode = self.sendCommandToItem(ohItem, brightValue)
 			else:
-				#find current item statusCode
+				# find current item statusCode
 				state = self.getCurrentItemStatus(ohItem)
 				if (state != None):
-					#dim or brighten the value
+					# dim or brighten the value
 					curBrightList = state.split(',')
-					curBright = int(curBrightList[len(curBrightList)-1])
+					curBright = int(curBrightList[len(curBrightList) - 1])
 
-					if(brightValue == None):
+					if (brightValue == None):
 						brightValue = "10"
 
-					#if ((command == "dim") or (command == "abbassa") or (command == "dimme") or (command == "oscurece")):
+					# if ((command == "dim") or (command == "abbassa") or (command == "dimme") or (command == "oscurece")):
 					if self.voc_match(command, 'Dim'):
-						newBrightValue = curBright-(int(brightValue))
+						newBrightValue = curBright - (int(brightValue))
 					else:
-						newBrightValue = curBright+(int(brightValue))
+						newBrightValue = curBright + (int(brightValue))
 
 					if (newBrightValue < 0):
 						newBrightValue = 0
@@ -271,7 +275,7 @@ class openHABSkill(MycroftSkill):
 					else:
 						pass
 
-					#send command to item
+					# send command to item
 					statusCode = self.sendCommandToItem(ohItem, str(newBrightValue))
 				else:
 					pass
@@ -295,10 +299,10 @@ class openHABSkill(MycroftSkill):
 		LOGGER.debug("Item: %s" % (messageItem))
 		requestType = message.data.get('requesttype')
 		LOGGER.debug("Request Type: %s" % (requestType))
-		
+
 		unitOfMeasure = self.translate('Degree')
 		infoType = self.translate('Temperature')
-		
+
 		self.currStatusItemsDic = dict()
 
 		if self.voc_match(requestType, 'Temperature'):
@@ -318,7 +322,8 @@ class openHABSkill(MycroftSkill):
 
 		if ohItem != None:
 			state = self.getCurrentItemStatus(ohItem)
-			self.speak_dialog('TempHumStatus', {'item': messageItem, 'temp_hum': infoType, 'temp_hum_val': state, 'units_of_measurement': unitOfMeasure})
+			self.speak_dialog('TempHumStatus', {'item': messageItem, 'temp_hum': infoType, 'temp_hum_val': state,
+												'units_of_measurement': unitOfMeasure})
 		else:
 			LOGGER.error("Item not found!")
 			self.speak_dialog('ItemNotFoundError')
@@ -341,9 +346,9 @@ class openHABSkill(MycroftSkill):
 				state = self.getCurrentItemStatus(ohItem)
 				if ((state != None) and (state.isdigit())):
 					if self.voc_match(command, 'Increase'):
-						newTempValue = int(state)+(int(tempVal))
+						newTempValue = int(state) + (int(tempVal))
 					else:
-						newTempValue = int(state)-(int(tempVal))
+						newTempValue = int(state) - (int(tempVal))
 
 					statusCode = self.sendCommandToItem(ohItem, str(newTempValue))
 				else:
@@ -363,19 +368,19 @@ class openHABSkill(MycroftSkill):
 			self.speak_dialog('ItemNotFoundError')
 
 	def sendStatusToItem(self, ohItem, command):
-		requestUrl = self.url+"/items/%s/state" % (ohItem)
+		requestUrl = self.url + "/items/%s/state" % (ohItem)
 		req = requests.put(requestUrl, data=command, headers=self.command_headers)
 
 		return req.status_code
 
 	def sendCommandToItem(self, ohItem, command):
-		requestUrl = self.url+"/items/%s" % (ohItem)
+		requestUrl = self.url + "/items/%s" % (ohItem)
 		req = requests.post(requestUrl, data=command, headers=self.command_headers)
 
 		return req.status_code
 
 	def getCurrentItemStatus(self, ohItem):
-		requestUrl = self.url+"/items/%s/state" % (ohItem)
+		requestUrl = self.url + "/items/%s/state" % (ohItem)
 		state = None
 
 		try:
@@ -395,5 +400,6 @@ class openHABSkill(MycroftSkill):
 	def stop(self):
 		pass
 
+
 def create_skill():
-    return openHABSkill()
+	return openHABSkill()
